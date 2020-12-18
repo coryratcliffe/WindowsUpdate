d101ca453b0a14ec834bff9075d5cab4c662e976
Cory Ratcliffe <46505198+coryratcliffe@users.noreply.github.com>
2020-12-18 11:40:28 -0600

    Add files via upload
    Simple update script

---
+++ Update.ps1
@@ -0,0 +1,8 @@
+﻿ 	
+Install-Module -Name PSWindowsUpdate
+
+sleep 3
+#Allow scripts to be ran in Power Shell by current user.
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+#Check for updates and install all of the available ones. 
+Get-WindowsUpdate -install -acceptall -autoreboot
